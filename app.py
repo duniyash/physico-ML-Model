49f5d0d24053e1ae3e38aae04b0ef0ae2b1e1d59
duniyash <80444721+duniyash@users.noreply.github.com>
2022-05-01 18:18:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,49 +11,50 @@ from sqlalchemy import true
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-model = load_model('psycho_model')
 
-im_w = 256
-im_h = 256
-
-psychoDict = {0: "correct", 1: "incorrect"}
-
-def convert_to_array(img):
-    im = cv2.imread(img)
-    img = Image.fromarray(im, 'RGB')
-    image = img.resize((im_w, im_h))
-    return np.array(image)
-
-def download_image_and_save(url):
-    f = open('psycho.jpg','wb')
-    f.write(urllib.request.urlopen(url).read())
-    f.close()
-
-@app.route('/url', methods=['POST'])
-def process_json():
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        json = request.json
-        download_image_and_save(json['url'])
-        imageArray = convert_to_array("psycho.jpg")
-        imageArray = imageArray/255
-        numpyImageArray = []
-        numpyImageArray.append(imageArray)
-        numpyImageArray = np.array(numpyImageArray)
-        prediction = model.predict(numpyImageArray)
-        label = np.argmax(prediction)
-        return jsonify(Response = "True", Prediction = psychoDict[label])
-    else:
-        return jsonify(Response = "False", Error = "Internal Error")
+# model = load_model('psycho_model')
+#
+# im_w = 256
+# im_h = 256
+#
+# psychoDict = {0: "correct", 1: "incorrect"}
+#
+# def convert_to_array(img):
+#     im = cv2.imread(img)
+#     img = Image.fromarray(im, 'RGB')
+#     image = img.resize((im_w, im_h))
+#     return np.array(image)
+#
+# def download_image_and_save(url):
+#     f = open('psycho.jpg','wb')
+#     f.write(urllib.request.urlopen(url).read())
+#     f.close()
+#
+# @app.route('/url', methods=['POST'])
+# def process_json():
+#     content_type = request.headers.get('Content-Type')
+#     if (content_type == 'application/json'):
+#         json = request.json
+#         download_image_and_save(json['url'])
+#         imageArray = convert_to_array("psycho.jpg")
+#         imageArray = imageArray/255
+#         numpyImageArray = []
+#         numpyImageArray.append(imageArray)
+#         numpyImageArray = np.array(numpyImageArray)
+#         prediction = model.predict(numpyImageArray)
+#         label = np.argmax(prediction)
+#         return jsonify(Response = "True", Prediction = psychoDict[label])
+#     else:
+#         return jsonify(Response = "False", Error = "Internal Error")
 
 @app.route("/")
 def test():
     return "Working"
 
-@app.route('/add', methods=['POST'])
-def add():
-    data = request.get_json()
-    return jsonify({'sum': data['a'] + data['b']})
+# @app.route('/add', methods=['POST'])
+# def add():
+#     data = request.get_json()
+#     return jsonify({'sum': data['a'] + data['b']})
 
 if __name__ == '__main__':
     app.run(debug=True)
